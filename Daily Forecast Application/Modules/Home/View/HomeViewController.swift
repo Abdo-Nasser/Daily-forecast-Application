cc3a0ae6f32aa9b7a1addd908da851d7eccabbd5
Abdelrahman Nasser <>
2021-08-13 19:56:35 +0200

    task #0003 - home ui design

--- Daily Forecast Application/Modules/Home/View/HomeViewController.swift
+++ Daily Forecast Application/Modules/Home/View/HomeViewController.swift
@@ -11,10 +11,28 @@ class HomeViewController: UIViewController {
     //MARK: - Attributes
     var presenter: HomePresenterProtocol?
     
+    //MARK: - Outlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var notAccurateView: UIView!
+    @IBOutlet weak var errorView: UIView!
+    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         presenter?.getCityWeather(cityName: "cairo")
+    }
+    
+    private func setupUI(){
+        
+    }
+    
+    //MARK: - Actions
+    @IBAction func searchAction(_ sender: Any) {
+    }
+    
+    @IBAction func retryAction(_ sender: Any) {
     }
 }
 
